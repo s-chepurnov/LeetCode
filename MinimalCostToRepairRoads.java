aebb37949c7b9413d4d68540b8a5b8e087089742
s-chepurnov <chepurnov.serge@gmail.com>
2019-09-21 20:10:21 +0700

    init MinimalCostToRepairRoads

---
+++ MinimalCostToRepairRoads.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Using a set of roads, broken roads and repair costs,
+ *write an algorithm to calculate the minimum total cost to repair some of the roads so that
+ *all the cities are once again accessible from each other.
+ *
+ *Input: The input consists of five arguments:
+ *numTotalAvailableCities: an integer representing the total number of cities (eg: if N = 3, then cities are represented as 1,2,3).
+ *numTotalAvailableRoads: an integer representing the total number of roads.
+ *roadsAvailable: a list of integers where each element of the list consists of a pair representing
+ *                the cities directly connected by a road.
+ *numRoadsToBeRepaired: an integer representing the total number of roads that are unusable.
+ *costRoadsToBeRepaired: a list of integers where each element of the list consists  of
+ *                       triplet representing the pair of cities between which a road is currently
+ *                       unusable and the cost of repairing that road, respectively
+ *                       (e.g. [1,3,10] means to repair a road between cities 1 and 3 the cost would be 10).
+ *
+ *Output: Return an integer representing the minimum total cost to repair some roads so that all
+ *the cities are accessible from each other.
+ *
+ * another solution: https://github.com/ajsn288github/Amazon/blob/master/Amazon1.java
+ */
+public class MinimalCostToRepairRoads {
+
+    public static void main(String[] args) {
+        // TODO: see the pictures Techlandia1 and Techlandia2 int the picture directory
+        // complete description
+        // describe an algorithm with all combinations
+        // compare with solution from ajsn288github
+        // ask a question if needed
+    }
+
+}
