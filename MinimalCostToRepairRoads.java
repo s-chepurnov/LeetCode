6ac7d21a7bd921b281bf14c038b132ad2aa447c9
s-chepurnov <chepurnov.serge@gmail.com>
2020-03-28 15:20:21 +0700

    mincostrepairroads

--- MinimalCostToRepairRoads.java
+++ MinimalCostToRepairRoads.java
@@ -60,18 +60,16 @@ class SolutionMinCostToRepairRoads {
 
         EdgeWeightedGraph g = new EdgeWeightedGraph(numTotalAvailableRoads + 1);
 
-        for (int[] edge : edges) {
-            int weight = findWeight(edge[0], edge[1], edgesToRepair);
-
-            //init Graph
-            g.addEdge(new Edge(edge[0], edge[1], weight));
-        }
-
+        //init Graph
 //        g.addEdge(new Edge(1,2,12));
 //        g.addEdge(new Edge(2,3,0));
 //        g.addEdge(new Edge(3,4,30));
 //        g.addEdge(new Edge(4,5,0));
 //        g.addEdge(new Edge(5,1,8));
+        for (int[] edge : edges) {
+            int weight = findWeight(edge[0], edge[1], edgesToRepair);
+            g.addEdge(new Edge(edge[0], edge[1], weight));
+        }
 
         UF uf = new UF(g.V);
 
@@ -91,9 +89,9 @@ class SolutionMinCostToRepairRoads {
 
     private int findWeight(int x, int y, int[][] edgesToRepair) {
         int weight = 0;
-        for (int[] edge : edgesToRepair) {
-            if(edge[0] == x && edge[1] == y) {
-                weight = edge[2];
+        for (int[] edgeToRepair : edgesToRepair) {
+            if(edgeToRepair[0] == x && edgeToRepair[1] == y) {
+                weight = edgeToRepair[2];
                 return weight;
             }
         }
@@ -212,24 +210,24 @@ class KruskalMST {
 
     public KruskalMST(EdgeWeightedGraph g, UF uf) {
         this.uf = uf;
-        PriorityQueue<Edge> minHeap = new PriorityQueue<Edge>(10, (o1, o2) -> {
-            return o1.compareTo(o2);
-        });
 
+        //1. Edges sorted by weight (asc, e.g. 1,2,3,4 ... n)
+        PriorityQueue<Edge> minHeap = new PriorityQueue<Edge>(10, Comparator.naturalOrder());
         for (Edge e : g.edges()) {
             minHeap.add(e);
         }
 
+        //2. connect edge only if it is not create a cycle
         while ( !minHeap.isEmpty() && mst.size() <= (g.V - 1) ) {
-            Edge e = minHeap.poll();
+            Edge edge = minHeap.poll();
 
-            int p = e.either();
-            int q = e.other(p);
+            int p = edge.either();
+            int q = edge.other(p);
 
-            if( !uf.connected(p,q)) {
+            if ( !uf.connected(p,q)) { //edge p-q does not create cycle
                 uf.union(p,q);
-                mst.offer(e);
-                weight += e.weight;
+                mst.offer(edge);
+                weight += edge.weight;
             }
         }
 
